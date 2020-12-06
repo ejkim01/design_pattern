c58f4bf03c7f025d65fddea4f5bb81f733173042
EUIJOONG KIM <sunwoodad@hotmail.com>
2020-12-06 10:29:36 -0800

    Command design pattern

---
+++ command.cpp
@@ -0,0 +1,75 @@
+/*
+	Command pattern is an Object behavioral pattern that decouples sender and receiver 
+	by encapsulating a request as an object, thereby letting you parameterize clients with different requests, 
+	queue or log requests, and support undo-able operations. It can also be thought as an object oriented equivalent of call back method.
+
+	Call Back: It is a function that is registered to be called at later point of time based on user actions.
+*/
+#include <iostream>
+
+/*the Command interface*/
+class Command {
+public:
+	virtual void execute() = 0;
+};
+
+/*Receiver class*/
+class Light {
+public:
+	Light() {}
+	void turnOn() {
+		cout << "The light is on" << endl;
+	}
+	void turnOff() {
+		cout << "The light is off" << endl;
+	}
+};
+
+/*the Command for turning on the light*/
+class FlipUpCommand : public Command {
+public:
+	FlipUpCommand(Light& light) : theLight(light) {}
+	virtual void execute() {
+		theLight.turnOn();
+	}
+private:
+	Light& theLight;
+};
+
+/*the Command for turning off the light*/
+class FlipDownCommand : public Command {
+public:
+	FlipDownCommand(Light& light) : theLight(light) {}
+	virtual void execute() {
+		theLight.turnOff();
+	}
+private:
+	Light& theLight;
+};
+
+class Switch {
+public:
+	Switch(Command& flipUpCmd, Command& flipDownCmd)
+		:flipUpCommand(flipUpCmd), flipDownCommand(flipDownCmd) {}
+
+	void flipUp() {
+		flipUpCommand.execute();
+	}
+
+	void flipDown() {
+		flipDownCommand.execute();
+	}
+private:
+	Command& flipUpCommand;
+	Command& flipDownCommand;
+};
+
+int main() {
+	Light lamp;
+	FlipUpCommand switchUp(lamp);
+	FlipDownCommand switchDown(lamp);
+
+	Switch s(switchUp, switchDown);
+	s.flipUp();
+	s.flipDown();
+}
